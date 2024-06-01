02dc40a5f7c145021327d22ee3c67372c5facd9a
Erickfer29 <erickdelgado0829@gmail.com>
2024-05-31 22:06:02 -0500

    Configuracion multer

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,19 +1,39 @@
 import { Router } from 'express';
 import pool from '../database.js'
+import multer from 'multer';
+import path from 'path'
 
 const router = Router();
+
+
+const storage = multer.diskStorage({
+    destination: 'src/public/uploads/',
+    filename: (req, file, cb) => {                          //Mayor o = 0 y Menor que 1
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        const ext = path.extname(file.originalname)
+        cb(null, file.fieldname + '-' + uniqueSuffix + ext)
+    }
+})
+
+const upload = multer({storage})
 
 router.get('/add', (req, res)=>{
     res.render('productos/add')
 });
 
-router.post('/add', async (req, res)=>{
+router.post('/add', upload.single('file') , async (req, res)=>{
     try {
         const { nombre, Descripcion, Precio, CantidadStock, Talla, Color, Categoria } = req.body
-        const newProducto = {
-            nombre, Descripcion, Precio, CantidadStock, Talla, Color, Categoria
+        let newProducto = {}
+        if(req.file){
+            const file = req.file
+            const imagen_original = file.originalname
+            const imagen = file.filename
+            newProducto = { imagen, nombre, Descripcion, Precio, CantidadStock, Talla, Color, Categoria }
+
+        }else{
+            newProducto = { nombre, Descripcion, Precio, CantidadStock, Talla, Color, Categoria }
         }
-        console.log(newProducto)
 
         await pool.query('INSERT INTO productos SET ?', [newProducto]);
         res.redirect('/list');
